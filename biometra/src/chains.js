a3cff2f1ce385f09091e6284b8751cc308fe18fc
Papagun <102505925+ShivgunGaming@users.noreply.github.com>
2023-07-18 11:20:22 +1000

    Chains got rid of sensitive info

--- biometra/src/chains.js
+++ biometra/src/chains.js
@@ -1,22 +1,8 @@
 const Ethereum = {
   hex: '0x1',
   name: 'Ethereum',
-  rpcUrl: 'https://eth-mainnet.g.alchemy.com/v2/Vv2y0ZW2JcOHZmAUdEEuvNlUxfxHtgs5',
+  rpcUrl: /* Enter your RPC url here */,
   ticker: 'ETH'
-};
-
-const Binance = {
-  hex: '0x38',
-  name: 'Binance Mainnet',
-  rpcUrl: 'https://bsc-dataseed.binance.org',
-  ticker: 'BNB'
-};
-
-const Avalanche = {
-  hex: '0xa86a',
-  name: 'Avalanche Mainnet',
-  rpcUrl: 'https://api.avax.network/ext/bc/C/rpc',
-  ticker: 'AVAX'
 };
 
 export const CHAINS_CONFIG = {
