62d7f46417c519a9345dd8536e5f59445ea5f598
Papagun <102505925+ShivgunGaming@users.noreply.github.com>
2023-08-19 13:34:56 +1000

    Added Multiple Chains, Avalanche C Chain and Binance Smart Chain

--- biometra/src/chains.js
+++ biometra/src/chains.js
@@ -1,8 +1,22 @@
 const Ethereum = {
   hex: '0x1',
   name: 'Ethereum',
-  rpcUrl: /* Enter your RPC url here */,
+  rpcUrl: 'https://eth-mainnet.g.alchemy.com/v2/Vv2y0ZW2JcOHZmAUdEEuvNlUxfxHtgs5',
   ticker: 'ETH'
+};
+
+const Binance = {
+  hex: '0x38',
+  name: 'Binance Mainnet',
+  rpcUrl: 'https://bsc-dataseed.binance.org',
+  ticker: 'BNB'
+};
+
+const Avalanche = {
+  hex: '0xa86a',
+  name: 'Avalanche Mainnet',
+  rpcUrl: 'https://api.avax.network/ext/bc/C/rpc',
+  ticker: 'AVAX'
 };
 
 export const CHAINS_CONFIG = {
