8520cea4c2e9a95284a70a5cf62496b750bd956d
Papagun <102505925+ShivgunGaming@users.noreply.github.com>
2023-08-19 13:57:18 +1000

    Added Chakra UI to index.js

--- biometra/src/index.js
+++ biometra/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { MemoryRouter } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <MemoryRouter>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </MemoryRouter>
   </React.StrictMode>
 );
