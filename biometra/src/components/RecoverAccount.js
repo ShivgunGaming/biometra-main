816dcc674deb4bf8faa2a8110cf5b16b66381aac
Papagun <102505925+ShivgunGaming@users.noreply.github.com>
2023-09-11 10:27:41 +1000

    Passage Element Display

--- biometra/src/components/RecoverAccount.js
+++ biometra/src/components/RecoverAccount.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Button, Input, Text, Textarea, VStack, Icon } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import { ethers } from "ethers";
+import PassageAuth from "./PassageAuth"; // Import your PassageAuth component
 
 const { TextArea } = Input;
 
@@ -9,24 +10,27 @@ function RecoverAccount({ setWallet, setSeedPhrase }) {
   const navigate = useNavigate();
   const [typedSeed, setTypedSeed] = useState("");
   const [nonValid, setNonValid] = useState(false);
+  const [showPassage, setShowPassage] = useState(false); // Control whether to show Passage
 
   const seedAdjust = (e) => {
     setNonValid(false);
     setTypedSeed(e.target.value);
   };
 
-  const recoverWallet = () => {
-    let recoveredWallet;
-    try {
-      recoveredWallet = ethers.Wallet.fromPhrase(typedSeed);
-    } catch (err) {
+  const handleSeedSubmission = () => {
+    // Check the seed phrase and show Passage if it's valid
+    if (typedSeed.split(" ").length === 12 && typedSeed.slice(-1) !== " ") {
+      setShowPassage(true);
+    } else {
       setNonValid(true);
-      return;
     }
+  };
 
-    setSeedPhrase(typedSeed);
-    setWallet(recoveredWallet.address);
-    navigate("/yourwallet");
+  const handleAuthenticate = (eventDetail) => {
+    // Handle authentication here, for example, by updating state or navigating to the wallet page.
+    console.log("User authenticated with Passage", eventDetail);
+    // You can access user details from eventDetail if needed.
+    // For example, eventDetail.user.email, eventDetail.user.name, etc.
   };
 
   return (
@@ -34,7 +38,7 @@ function RecoverAccount({ setWallet, setSeedPhrase }) {
       <VStack align="center">
         <Icon boxSize={8} color="red.500" />
         <Text>
-          Enter your 12 word seed phrase, seperated by spaces below to log in!
+          Enter your 12-word seed phrase, separated by spaces below to log in!
         </Text>
       </VStack>
       <Textarea
@@ -53,7 +57,7 @@ function RecoverAccount({ setWallet, setSeedPhrase }) {
         colorScheme="pink"
         size="lg"
         className="frontPageRecoverButton"
-        onClick={recoverWallet}
+        onClick={handleSeedSubmission}
       >
         Recover Wallet
       </Button>
@@ -72,6 +76,11 @@ function RecoverAccount({ setWallet, setSeedPhrase }) {
       >
         Back Home
       </Text>
+
+      {/* Conditionally render the PassageAuth component */}
+      {showPassage && (
+        <PassageAuth appId="NOT_YOUR_BUSINESS" onAuthenticate={handleAuthenticate} />
+      )}
     </VStack>
   );
 }
